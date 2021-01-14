1a247fae83204bf958f2e9bdc8f6cce8c6104557
SnSation <blair95273@gmail.com>
2021-01-14 13:24:34 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import Home from './views/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <div className="PageContainer">
+          <Switch>
+            <Route exact path='/' render={ () => <Home /> } />
+          </Switch>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h3>Home Page</h3>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Home;
